f7d6cea16b2509ea583d17689a3cb5d2e48dc5d7
GotaX <g.tianxiang@gmail.com>
2018-06-30 15:36:42 +0800

    Add config loader

---
+++ conf/conf.go
@@ -0,0 +1,71 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
+)
+
+const (
+	kAppName       = "APP_NAME"
+	kConfigServer  = "CONFIG_SERVER"
+	kConfigLabel   = "CONFIG_LABEL"
+	kConfigProfile = "CONFIG_PROFILE"
+	kConfigType    = "CONFIG_TYPE"
+)
+
+var (
+	App AppConfig
+)
+
+type AppConfig struct {
+	Name      string
+	SearchUrl string `mapstructure:"search_url"`
+}
+
+func init() {
+	viper.AutomaticEnv()
+	initDefault()
+
+	if err := loadRemoteConfig(); err != nil {
+		log.Fatal("Fail to load config", err)
+	}
+
+	if err := sub("app", &App); err != nil {
+		log.Fatal("Fail to parse config", err)
+	}
+}
+
+func initDefault() {
+	viper.SetDefault(kAppName, "go-app")
+	viper.SetDefault(kConfigServer, "http://localhost:8080")
+	viper.SetDefault(kConfigLabel, "config")
+	viper.SetDefault(kConfigProfile, "default")
+	viper.SetDefault(kConfigType, "YAML")
+}
+
+func loadRemoteConfig() (err error) {
+	confAddr := fmt.Sprintf("%v/%v/%v-%v.yml",
+		viper.Get(kConfigServer), viper.Get(kConfigLabel),
+		viper.Get(kAppName), viper.Get(kConfigProfile))
+	resp, err := http.Get(confAddr)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	viper.SetConfigType(viper.GetString(kConfigType))
+	if err = viper.ReadConfig(resp.Body); err != nil {
+		return
+	}
+	log.Println("Load config from: ", confAddr)
+	return
+}
+
+func sub(key string, value interface{}) error {
+	sub := viper.Sub(key)
+	sub.AutomaticEnv()
+	sub.SetEnvPrefix(key)
+	return sub.Unmarshal(value)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/GotaX/config-server-demo/conf"
 	"log"
 	"net/http"
 )
@@ -9,7 +10,7 @@ import (
 func main() {
 	http.HandleFunc("/search", func(w http.ResponseWriter, req *http.Request) {
 		q := req.URL.Query().Get("q")
-		src := fmt.Sprintf("https://cn.bing.com/search?q=%v", q)
+		src := fmt.Sprintf(conf.App.SearchUrl, q)
 		fmt.Fprintf(w, `<iframe width="100%%" height="98%%" scrolling="auto" frameborder="0" src="%v">`, src)
 	})
 	log.Fatal(http.ListenAndServe(":8081", nil))
